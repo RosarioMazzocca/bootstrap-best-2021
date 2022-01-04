c9047efbb4aaf426bf4c317d4dc32ea309684fe5
RosarioMazzocca <mazzocca.rosario@libero.it>
2022-01-04 15:42:38 +0100

    Pubblicato programma finale

--- src/main/java/org/generation/italy/controller/IndexController.java
+++ src/main/java/org/generation/italy/controller/IndexController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+// Gruppo 1: Rosario Mazzocca, Roberta Angioni, Alessandro Caraffa, Gianluca Pensa
+
 @Controller 
 @RequestMapping("/")
 public class IndexController {
@@ -22,8 +24,6 @@ public class IndexController {
 		}
 		
 		model.addAttribute("name", "Rosario");
-		
-		
 		
 		return "index";
 	}
